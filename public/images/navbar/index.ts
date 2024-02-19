bdf067d6929cd3d87e4e3cb2d0d41badaadd5709
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-02-19 20:53:50 +0900

    💄 style : Add navbar-related images and icons
    - Add user images for testing
    - Add the icon svg file needed for the navbar
    - Manage images and svg you add by adding an index file
    
    Related issue: #33

---
+++ public/images/navbar/index.ts
@@ -0,0 +1,1 @@
+export const user = '/images/navbar/user.png';
---
+++ public/svgs/navbar/index.ts
@@ -0,0 +1,7 @@
+export const book = '/svgs/navbar/book.svg';
+export const group = '/svgs/navbar/group.svg';
+export const door = '/svgs/navbar/door.svg';
+export const moreHoriz = '/svgs/navbar/moreHoriz.svg';
+export const moreVert = '/svgs/navbar/moreVert.svg';
+export const mypage = '/svgs/navbar/mypage.svg';
+export const search = '/svgs/navbar/search.svg';
