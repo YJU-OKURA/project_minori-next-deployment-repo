6c775f8aa4968472d5929cdf6ba3622544afccfe
Lainari <108247620+Lainari@users.noreply.github.com>
2024-02-27 10:37:37 +0900

    💄 style : Add images & icons to put in Card Components
    - Add example images and icons as png and svg files
    
    Related issue: #59

---
+++ public/images/group/index.ts
@@ -0,0 +1,10 @@
+const logos = {
+  seouluni: '/images/group/seouluni.png',
+  twayair: '/images/group/twayair.png',
+  wasedauni: '/images/group/wasedauni.png',
+  yahoo: '/images/group/yahoo.png',
+  yeungjin: '/images/group/yeungjin.png',
+  github: '/images/group/github.png',
+};
+
+export default logos;
