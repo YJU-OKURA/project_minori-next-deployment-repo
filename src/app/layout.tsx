1bac0937d094dbad94d9740ae17bb54d2f28f5b3
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-03-05 15:37:26 +0900

    ✨ feat : Add a Footer component to your web page
    You enhanced the user experience by adding a Footer component to your web page, which provides information about the web page.
    
    Related issue: #72

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import {Navbar} from '../components/navbar';
+import {Footer} from '../components/footer';
 import './globals.css';
 
 const inter = Inter({subsets: ['latin']});
@@ -18,6 +19,7 @@ const RootLayout = ({children}: {children: React.ReactNode}) => {
           <Navbar />
           <div className="grow overflow-x-auto overflow-y-auto pl-72">
             {children}
+            <Footer />
           </div>
         </div>
       </body>
