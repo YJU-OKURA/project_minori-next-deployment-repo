c88ca4d80d6046a1aca1192507708f731c78313a
Lainari <108247620+Lainari@users.noreply.github.com>
2024-03-07 11:17:09 +0900

    💄 style : Add icons used within class pages
    - Added to index.ts to manage within one folder
    
    Related issue: #77

--- public/svgs/group/index.ts
+++ public/svgs/group/index.ts
@@ -2,6 +2,14 @@ const icons = {
   favorite: 'svgs/group/favorite.svg',
   folder: '/svgs/group/folder.svg',
   noneFavorite: 'svgs/group/noneFavorite.svg',
+  join: 'svgs/group/join.svg',
+  create: 'svgs/group/create.svg',
+  notice: 'svgs/group/notice.svg',
+  thumbnail: 'svgs/group/thumbnail.svg',
+  dropdownButton: 'svgs/group/dropdownButton.svg',
+  post: 'svgs/group/post.svg',
+  moreVert: 'svgs/group/moreVert.svg',
+  addButton: 'svgs/group/addButton.svg',
 };
 
 export default icons;
