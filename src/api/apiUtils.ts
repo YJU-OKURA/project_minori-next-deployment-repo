a38b53921f764de01ad710c50e6b98576e53f641
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-03-15 09:26:41 +0900

    ♻️ refactor : Added ability to set API request URLs in apiUtils
    Added the ability to dynamically set the API request URL based on values received via props
    
    Related issue: #91

--- src/api/apiUtils.ts
+++ src/api/apiUtils.ts
@@ -1,18 +1,25 @@
 import axios from 'axios';
 import {HttpStatus} from './httpStatus';
 
-const BASE_URL = 'http://43.203.66.25/api/gin';
-// const BASE_URL = 'http://3.38.86.236:3000/api/nest';
+const BASE_URLS = {
+  gin: 'http://43.203.66.25/api/gin',
+  nest: 'http://3.38.86.236:3000/api/nest',
+};
 
-const api = axios.create({
-  baseURL: BASE_URL,
-});
+const api = axios.create();
 
 const req = async (
   url: string,
   method: string,
+  server: 'gin' | 'nest',
   body: object | undefined = undefined
 ) => {
+  api.defaults.baseURL = BASE_URLS[server];
+  if (server === 'nest') {
+    api.defaults.headers.common['Authorization'] =
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZXhwIjoxOTY5OTAxMDIyfQ.U0k1q2oTrp3JwsIpem16o2W77tpVGiwylwc5cTFaZgU';
+  }
+
   if (typeof body !== 'undefined') {
     api.defaults.headers.common['Content-Type'] = 'application/json';
   }
@@ -29,7 +36,7 @@ const req = async (
 
     return response.data;
   } catch (error) {
-    console.error(`${method} リクエスト中にエラーが発生しました:`, error);
+    console.error(`${method} リクエスト中にエラーが発生しました.`, error);
     throw error;
   }
 };
