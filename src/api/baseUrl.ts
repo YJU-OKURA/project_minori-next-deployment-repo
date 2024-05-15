2f5bc3f3c97ebf54a3f8297fbed7f090fe600c41
Regulus0811 <kbc6268@g.yju.ac.kr>
2024-05-16 00:57:52 +0900

    ♻️ refactor: Update base URL for connecting to the Gin server
    Adjusted the base URL used for connecting to the Gin server.
    
    Related issue: #138

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,6 +1,8 @@
 const BASE_URLS = {
+  // gin: 'https//minoriedu.com',
   gin: 'https://minoriedu.com/api/gin',
-  nest: 'http://3.38.86.236:3000/api/nest',
+  // nest: 'http://3.38.86.236:3000/api/nest',
+  nest: 'https://minoriedu.com/api/nest',
 };
 
 export default BASE_URLS;
