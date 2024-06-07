895e47847ff76e492c2cce875dcb0d393c181e8d
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-06-07 14:02:22 +0900

    💄 style : Add images to your quiz page
    - Add images required for quiz grading
    - Add icons required for quiz creation
    - Manage images and icons using an index file
    
    Related Issue: #102

---
+++ public/images/quiz/index.ts
@@ -0,0 +1,6 @@
+const pngs = {
+  true: '/images/quiz/true.png',
+  false: '/images/quiz/false.png',
+};
+
+export default pngs;
---
+++ public/svgs/quiz/index.ts
@@ -0,0 +1,8 @@
+const icons = {
+  normal: '/svgs/quiz/normal.svg',
+  more: '/svgs/quiz/more.svg',
+  delete: '/svgs/quiz/delete.svg',
+  plus: '/svgs/quiz/plus.svg',
+};
+
+export default icons;
