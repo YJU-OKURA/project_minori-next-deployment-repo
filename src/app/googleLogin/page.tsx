527d661a1b1a12a26b70bb7ce84c7480b610f44c
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-10-23 20:05:31 +0900

    ♻️ refactor : Update Google Login page for client-side rendering
    - Modified the Google Login page to use URLSearchParams for obtaining the 'code' parameter from the URL.
    - Implemented `getServerSideProps` to disable static generation, ensuring the page renders on the client side.
    - Added error handling for login response to improve user state management and cookie handling.
    
    Related issue: #129

--- src/app/googleLogin/page.tsx
+++ src/app/googleLogin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {useEffect} from 'react';
 import Image from 'next/image';
-import {useRouter, useSearchParams} from 'next/navigation';
+import {useRouter} from 'next/navigation';
 import Cookies from 'js-cookie';
 import {useSetRecoilState} from 'recoil';
 import userState from '@/src/recoil/atoms/userState';
@@ -9,11 +9,16 @@ import postGoogleLogin from '@/src/api/auth/postGoogleLogin';
 import gifs from '@/public/gif';
 import '@/src/styles/variable.css';
 
+export const dynamic = 'force-dynamic'; // 이 페이지는 클라이언트 측에서만 렌더링됨
+
 const Page = () => {
-  const code: string | null = useSearchParams().get('code');
   const setUser = useSetRecoilState(userState);
   const router = useRouter();
+
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+
     if (code) {
       postGoogleLogin(code).then(res => {
         if (res.user) {
@@ -26,7 +31,7 @@ const Page = () => {
         router.push('/classes');
       });
     }
-  }, []);
+  }, [router, setUser]);
 
   return (
     <div className="absolute top-0 opacity-50 w-full h-[100vh] flex justify-center items-center bg-[#8c8c8c]">
