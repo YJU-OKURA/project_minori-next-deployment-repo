29130f748f5c9536ca50cf5e87806bc6381c7e50
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2024-10-23 19:18:25 +0900

    ✨ feat : Install additional libraries
    Modify files based on library installation
    
    Related issue: #33

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/nest/:path*', // :path*로 와일드카드 매칭
+        destination: 'http://localhost:3001/api/nest/:path*', // 매칭된 path를 전달
+      },
+      {
+        source: '/api/gin/:path*', // :path*로 와일드카드 매칭
+        destination: 'http://localhost:80/api/gin/:path*', // 매칭된 path를 전달
+      },
+    ];
+  },
   webpack: config => {
     config.resolve.alias.canvas = false;
 
